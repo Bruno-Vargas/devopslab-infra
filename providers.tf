41519aa6be1535f9fda91352802f2e720e3c24b9
Bruno-Vargas <brunoversignassi@gmail.com>
2021-05-26 18:51:48 -0300

    removing creditials from providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "google" {
- # credentials = file("/home/vagrant/lab-devops-cloud-314617-2cc3fc945047.json")
   project     = "lab-devops-cloud-314617"
   region      = "us-central1"
   zone        = "us-central1-c"
